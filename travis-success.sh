a2e85ef62bb5daf71d8b5284e9455f2415d7f8f4
Syed Ali Murtaza <smurtazakazmi14@gmail.com>
2019-07-23 00:40:07 +0500

    # WIP docker login issue

--- travis-success.sh
+++ travis-success.sh
@@ -3,7 +3,7 @@
 #### halt script on error
 set -xe
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
 #### Push the Docker Images
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
