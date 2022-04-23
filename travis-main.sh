7f5bdf13d959154d6838abe9ba86770190628644
Syed Ali Murtaza <ali@kazmi.me>
2022-04-23 14:21:05 +0500

    Moves CI from travis to GitHub Actions, Adds support for php 8.1

--- travis-main.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#### halt script on error
-set -xe
-
-#### Build the Docker Images
-if [ -n "${PHP_VERSION}" ]; then
-    docker build -f ./Dockerfile-${PHP_VERSION} -t ${DOCKER_ORGANIZATION}/base-php:${IMAGE_TAG} .
-fi
